035b6af924c22b7cddd12aa905d4502862a75a82
student <dagon.samagon@gmail.com>
2024-01-31 16:08:53 +0300

    add lint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,13 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import meet from '../src/cli.js';
 
-
-console.log('Welcome to the Brain games!');
-const name = readlineSync.question('May I have your name?');
-console.log(`Hello, ${name}!`)
-
-
-
-
-
-// sudo apt-get install npm
+ meet();
