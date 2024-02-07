8fa42fa198051b2aff2bfddeb6ed50690fe5b8e5
student <dagon.samagon@gmail.com>
2024-02-07 12:07:09 +0300

    evenGame

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const getMeeting = () => {
-  console.log('Welcome to the Brain games!');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
+  return name; 
 };
 
 export default getMeeting;
